59738d557d20cba0d98db2937b0733d77faa52c6
Maesca06 <manuela.calero@correounivalle.edu.co>
2025-06-27 16:42:39 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Inicializar el mapa en Cali
+// Crear el mapa centrado en Cali
 var map = L.map('map').setView([3.4372, -76.5225], 13);
 
 // Capa base OSM
@@ -7,5 +7,27 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19
 }).addTo(map);
 
-// Puedes agregar aquí controles adicionales como escala o ubicación
-L.control.scale().addTo(map);
+// Botón "Volver al inicio"
+function volverInicio() {
+  map.setView([3.4372, -76.5225], 13);
+}
+
+// Función para cargar una capa GeoJSON desde el menú
+let capaActual;
+
+function cargarCapa(ruta) {
+  if (capaActual) {
+    map.removeLayer(capaActual);
+  }
+  if (ruta) {
+    fetch(ruta)
+      .then(res => res.json())
+      .then(data => {
+        capaActual = L.geoJSON(data).addTo(map);
+        map.fitBounds(capaActual.getBounds());
+      })
+      .catch(err => alert("Error al cargar la capa: " + err));
+  }
+}
+
+
