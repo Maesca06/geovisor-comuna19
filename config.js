c70a7f7e6969c94b98406d42c44af35c57178be3
Maesca06 <manuela.calero@correounivalle.edu.co>
2025-07-13 11:45:27 -0500

    Add files via upload

--- config.js
+++ config.js
@@ -1,44 +1,44 @@
 const layers = [
   {
     name: "Comuna 19",
-    url: "http://ec2-54-236-112-99.compute-1.amazonaws.com:8080/geoserver/movilidadcomuna19/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=movilidadcomuna19:comuna19&outputFormat=application/json",
+    url: "Capas GEO/Comuna 19.geojson",
     style: { color: "#000000", weight: 3, fillColor: "#ccffcc", fillOpacity: 0.2 },
     pane: "comunaPane",            
   interactive: false
   },
   {
     name: "Vías",
-    url: "http://ec2-54-236-112-99.compute-1.amazonaws.com:8080/geoserver/movilidadcomuna19/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=movilidadcomuna19:calles&outputFormat=application/json",
+    url: "Capas GEO/Vias.geojson",
     style: { color: "#666666", weight: 1 }
   },
   {
     name: "Ciclorrutas",
-    url: "http://ec2-54-236-112-99.compute-1.amazonaws.com:8080/geoserver/movilidadcomuna19/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=movilidadcomuna19:ciclorutas&outputFormat=application/json",
+    url: "Capas GEO/Ciclorutas.geojson",
     style: { color: "#0099cc", weight: 2 }
   },
   {
     name: "Estaciones del MIO",
-    url: "http://ec2-54-236-112-99.compute-1.amazonaws.com:8080/geoserver/movilidadcomuna19/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=movilidadcomuna19:estaciones%20del%20mio&outputFormat=application/json",
+    url: "Capas GEO/Estaciones de MIO.geojson",
     style: { color: "#003366", weight: 1, fillColor: "#003366", fillOpacity: 0.5 }
   },
   {
     name: "Paradas de MIO",
-    url: "http://ec2-54-236-112-99.compute-1.amazonaws.com:8080/geoserver/movilidadcomuna19/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=movilidadcomuna19:paradas%20de%20mio&outputFormat=application/json",
+    url: "Capas GEO/Paradas de MIO.geojson",
     pointStyle: { radius: 3, color: "#000000", fillColor: "#33cc33", fillOpacity: 1, weight: 1 }
   },
   {
     name: "Tramos Restricciones Viales",
-    url: "http://ec2-54-236-112-99.compute-1.amazonaws.com:8080/geoserver/movilidadcomuna19/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=movilidadcomuna19:tramos_restricciones_viales&outputFormat=application/json",
+    url: "Capas GEO/Tramos con restriciones viales.geojson",
     style: { color: "#FF0000", weight: 4 }
   },
   {
     name: "Áreas de Alta Demanda",
-    url: "http://ec2-54-236-112-99.compute-1.amazonaws.com:8080/geoserver/movilidadcomuna19/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=movilidadcomuna19:areas_alta_demanda&outputFormat=application/json",
+    url: "Capas GEO/Áreas con alta demanda.geojson",
     style: { color: "#FFFF66", weight: 1, fillColor: "#FFFF66", fillOpacity: 0.4 }
   },
   {
     name: "Puntos Semáforos Dañados",
-    url: "http://ec2-54-236-112-99.compute-1.amazonaws.com:8080/geoserver/movilidadcomuna19/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=movilidadcomuna19:puntos_semaforos_danados&outputFormat=application/json",
+    url: "Capas GEO/Puntos de semaforos dañados.geojson",
     pointStyle: { radius: 6, color: "#000000", fillColor: "#FF0000", fillOpacity: 1, weight: 1 }
   }
 ];
